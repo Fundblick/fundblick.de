e713546f4c7fc935f290b0e8072de29bab504cdc
Fundblick <jensdanhel@gmx.de>
2026-09-25 23:37:15 +0200

    Harden ADCELL activation flags

--- affiliate-config.js
+++ affiliate-config.js
@@ -5,14 +5,16 @@
   if(root)root.FundBlickAffiliateConfig=config;
 })(typeof window!=='undefined'?window:null,function(){
   return Object.freeze({
-    version:1,
+    version:2,
     network:'ADCELL',
+    mode:'prepared',
     adcell:Object.freeze({
       enabled:false,
+      outboundEnabled:false,
       trackingEnabled:false,
       liveDisclosure:false,
       vendorId:766,
-      consentVersion:'2026-09-25',
+      consentVersion:'2026-09-25-v2',
       consentStorageKey:'fundblick-adcell-consent-v1',
       privacyUrl:'https://www.adcell.de/datenschutz'
     })
